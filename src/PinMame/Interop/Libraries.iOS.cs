650c9e5e9ec3de03c1e1c7533220536947b8204d
Jason Millard <jsm174@gmail.com>
2022-01-24 18:47:02 -0500

    misc: update to use static libpinmame for iOS

--- src/PinMame/Interop/Libraries.iOS.cs
+++ src/PinMame/Interop/Libraries.iOS.cs
@@ -33,6 +33,6 @@ namespace PinMame.Interop
 {
 	internal static partial class Libraries
 	{
-		internal const string PinMame = "libpinmame.3.5.dylib";
+		internal const string PinMame = "__Internal";
 	}
 }
